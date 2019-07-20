11ac903dec5143470ba5c9bf70628e13dac33aa5
matacz <matacz@gmail.com>
2019-07-20 11:44:42 +0200

    Initial commit

---
+++ src/com/jsp/servlet/DateServlet.java
@@ -0,0 +1,29 @@
+package com.jsp.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet(name = "DateServlet", urlPatterns = {"/date"})
+public class DateServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        DateFormat currentDate = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        {
+            Date date = new Date();
+            out.println("aktualnie data i godzina: " + currentDate.format(date));
+        }
+    }
+}
